555bbc4bed6c82ef4350c73993640d8cbaaee087
Richard Garcia <code.richardy@gmail.com>
2026-01-06 20:08:06 -0300

    refactor: remove unused Permissions class to streamline codebase

--- Source/Vinder.Comanda.Orders.CrossCutting/Constants/Permissions.cs
+++
@@ -1,8 +0,0 @@
-namespace Vinder.Comanda.Orders.CrossCutting.Constants;
-
-public static class Permissions
-{
-    public const string ViewOrders = "permissions.orders.view";
-    public const string CreateOrder = "permissions.orders.create";
-    public const string UpdateOrder = "permissions.orders.update";
-}
